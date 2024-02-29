02285c62bb946964acaeea3a4358f6f897dccf6e
Srni Vasireddi <vsraouk@gmail.com>
2024-02-29 20:35:50 +0000

    BRANCH-2 #added tests first TDD, for 'Apple cost 60p and Oranges cost 25p' and the tests fails initially

---
+++ src/main/scala/com/tddshop/exercise/checkout/Checkout.scala
@@ -0,0 +1,13 @@
+package com.tddshop.exercise
+package com.tddshop.exercise.checkout
+import com.tddshop.exercise.products.Product
+
+class Checkout {
+  def Total(inputProducts: List[Product]): Double = {
+    var result = 0.0
+
+   result
+  }
+
+}
+
